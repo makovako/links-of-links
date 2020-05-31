6673e472a5257963386ce49ea08fe176b4037900
Marek Valko <valko.marek@gmail.com>
2020-05-31 20:49:28 +0200

    Toggle description

--- pages/components/Link.js
+++ pages/components/Link.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 export default function Link({link}) {
+    const [description, setDescription] = useState(false);
+
+    function toggleDescription(e) {
+        e.preventDefault()
+        setDescription(!description)
+    }
+
     return (
         <a target="_self" href={link.url} norefferer="true" noopener="true" nofollow="true">
-            <h2>{link.title}</h2>
+            <h2>{link.title}<span>x</span></h2>
             <p>{link.excerpt}</p>
+            {description ? (<p>{link.description}</p>) : null}
+            <span onClick={toggleDescription}>{description ? "x" : "i"}</span>
         </a>
     )
 }
